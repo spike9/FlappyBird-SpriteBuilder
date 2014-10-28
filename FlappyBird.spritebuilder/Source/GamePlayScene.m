9ff0bc79fa85c387efd8851b0e1219666206fbb3
spike9 <d.wyss@gmx.ch>
2014-10-28 19:12:42 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -6,7 +6,9 @@
 
 - (void)initialize
 {
-    // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+    
 }
 
 -(void)update:(CCTime)delta
